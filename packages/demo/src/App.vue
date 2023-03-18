3a3f830625873305d607f8d58fbeeaff265d2304
Kirill Stryaponov <stryaponoff@gmail.com>
2023-03-18 15:12:15 +0600

    Update demo project - Add path aliases - Update layout - Add sfc type shims

--- packages/demo/src/App.vue
+++ packages/demo/src/App.vue
@@ -1,12 +1,34 @@
 <template>
   <div class="container">
-    <div class="col">
-      <textarea v-model="code"></textarea>
-      <div>Result: {{ result }}</div>
+    <div class="row">
+      <div class="col">
+        <div class="row">
+          <h2>AST</h2>
+        </div>
+        <div class="row">
+          <div class="col">
+            <textarea v-model="code"></textarea>
+            <div>Result: {{ result }}</div>
+          </div>
+          <div class="col">
+            <vue-json-pretty v-if="!parsed.isError" :data="parsed.value" />
+            <code class="has-error" v-else>{{ parsed.value }}</code>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="col">
-      <vue-json-pretty v-if="!parsed.isError" :data="parsed.value" />
-      <code class="has-error" v-else>{{ parsed.value }}</code>
+
+    <div class="row">
+      <div class="col">
+        <div class="row">
+          <h2>Editor Component</h2>
+        </div>
+        <div class="row">
+          <div class="col">
+            <editor />
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -16,16 +38,18 @@ import VueJsonPretty from 'vue-json-pretty';
 import 'vue-json-pretty/lib/styles.css';
 import { Interpreter, Parser } from 'calc-parser';
 import { computed, defineComponent, ref } from "vue";
+import Editor from '@/components/Editor.vue'
 
 export default defineComponent({
   components: {
+    Editor,
     VueJsonPretty,
   },
   setup() {
     const parser = new Parser()
     const interpreter = new Interpreter()
 
-    const code = ref('')
+    const code = ref('1 + (2 - 3) + 4')
     const parsed = computed(() => {
       try {
         const value = parser.parse(code.value)
@@ -69,6 +93,12 @@ export default defineComponent({
 }
 
 .container {
+  display: flex;
+  flex-direction: column;
+}
+
+.row {
+  flex: 1;
   display: flex;
   flex-direction: row;
 }
---
+++ packages/demo/src/components/Editor.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>{{ JSON.stringify(lines) }}</div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const editorText = ref('')
+    const lines = computed<string[]>(() => {
+      return []
+    })
+
+    return {
+      editorText,
+      lines,
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+</style>
